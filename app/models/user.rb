26cf5ce8fa886e26d1d59471b0a1e9038d4de6fa
renaman <n.renaman@iclound.com>
2020-07-15 14:43:43 +0900

    フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,27 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  def follow(user_id)
+  	follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+  	unfollow.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+  	following_user.include?(user)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 	devise_for :users
 	root 'home#top'
   	get 'home/about'
-  	resources :users,only: [:show,:index,:edit,:update]
+  	resources :users,only: [:show,:index,:edit,:update] do
+  		resource :relationships, only: [:create, :destroy]
+  		get "follows" => "relationships#follower", as: "follows"
+  		get "follower" => "relationships#followed", as: "followers"
+  	end
   	resources :books do
   		resource :favorites, only: [:create, :destroy]
   		resource :book_comments, only: [:create, :destroy]
